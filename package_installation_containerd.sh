d242268256d9fc8d616b5017f3e9ee5622efb00e
chan18 <p.chan20new@gmail.com>
2024-05-04 22:01:04 +0530

    Setting up containerd is done on master node

--- package_installation_containerd.sh
+++ package_installation_containerd.sh
@@ -17,7 +17,6 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-# sysctl params required by setup, params persist across reboots
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
@@ -25,21 +24,16 @@ net.ipv4.ip_forward                 = 1
 EOF
 
 
-# Apply sysctl params without reboot
 sudo sysctl --system
 
 
-#Install containerd...
 sudo apt-get install -y containerd
 
 
-#Create a containerd configuration file
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 
 
-#Set the cgroup driver for containerd to systemd which is required for the kubelet.
-#For more information on this config file see:
 # https://github.com/containerd/cri/blob/master/docs/config.md and also
 # https://github.com/containerd/containerd/blob/master/docs/ops.md
 
@@ -49,19 +43,15 @@ sudo containerd config default | sudo tee /etc/containerd/config.toml
           [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
             SystemdCgroup = true
 
-#You can use sed to swap in true
 sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 
 
-#Verify the change was made
 grep 'SystemdCgroup = true' /etc/containerd/config.toml
 
-
-#Restart containerd with the new configuration
 sudo systemctl restart containerd
 
 
-
+---
 
 #Install Kubernetes packages - kubeadm, kubelet and kubectl
 #Add k8s.io's apt repository gpg key, this will likely change for each version of kubernetes release. 
